6f0d3413ec0fa80315f04ee0c9f2c1204563267a
Max Zheng <mzheng@confluent.io>
2017-07-10 23:21:09 -0700

    Initial AIO web app

---
+++ manage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+
+import click
+
+from app.management import prepare_database
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command(help='Run web app')
+def runserver():
+    os.system('adev runserver --host `hostname`')
+
+
+@main.command(help='Initialize database')
+def initdb():
+    prepare_database(delete_existing=True)
+
+
+if __name__ == '__main__':
+    main()
